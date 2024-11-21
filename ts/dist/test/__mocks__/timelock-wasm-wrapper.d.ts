caed8ae09ae528c3b1e9a2d0848a979e6d58c970
driemworks <driemworks@idealabs.network>
2024-11-21 09:53:56 -0600

    chore: update gitingore, remove extra files

--- ts/dist/test/__mocks__/timelock-wasm-wrapper.d.ts
+++
@@ -1,8 +0,0 @@
-/// <reference types="jest" />
-declare const init: jest.Mock<any, any, any>;
-declare const build_encoded_commitment: jest.Mock<any, any, any>;
-declare const tle: jest.Mock<any, any, any>;
-declare const tld: jest.Mock<any, any, any>;
-declare const decrypt: jest.Mock<any, any, any>;
-export default init;
-export { build_encoded_commitment, tle, tld, decrypt };
